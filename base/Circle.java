965a312416c806a563bdbe1a01011041f6e84d96
leekayon <lky1732@gmail.com>
2019-09-09 14:15:39 +0900

    Circle 오류 수정

--- base/Circle.java
+++ base/Circle.java
@@ -1,9 +1,11 @@
 package base;
 
-
- * @author ((2018315036 양유석 , 2018315030 이가영 , 2018315022 이지연))
- * @version (a version number or a date)
- */
+ /*
+  * 
+  * 
+  * @author ((2018315036 양유석 , 2018315030 이가영 , 2018315022 이지연))
+  * @version (a version number or a date)
+  */
 public class Circle
 {
     private int x;
