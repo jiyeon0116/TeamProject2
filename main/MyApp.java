5f71e74ed13a1c8dd599061d5f66901b6c5190dc
jiyeon <gbko159@naver.com>
2019-09-09 14:11:48 +0900

    merge

--- main/MyApp.java
+++ main/MyApp.java
@@ -1,3 +1,4 @@
+
 package main;
 import base.Circle;
 
@@ -11,7 +12,6 @@ public class MyApp{
     public static void main(String[] args){
         Circle c1 = new Circle(2, 3, 5);
         Circle c2 = new Circle(2, 3, 10);
-
         System.out.println("원1 : " + c1);
         System.out.println("원2 : " + c2);
         if(c1.equals(c2))
