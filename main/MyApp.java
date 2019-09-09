007fbb6d372baa6b539a1c2d98085a9b96e9e4ab
jiyeon <gbko159@naver.com>
2019-09-09 14:02:48 +0900

    기본 틀 작성

--- main/MyApp.java
+++ main/MyApp.java
@@ -7,29 +7,15 @@ package main;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyApp
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyApp
-     */
-    public MyApp()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class MyApp{
+    public static void main(String[] args){
+        Circle c1 = new Circle(2, 3, 5);
+        Circle c2 = new Circle(2, 3, 10);
+        System.out.println(“원1 : ” + c1);
+        System.out.println(“원2 : ” + c2);
+        if(c1.equals(c2))
+            System.out.println(“같은 원”);
+        else
+            System.out.println(“서로 다른 원”);
     }
 }
--- myInterface/Shape.java
+++ myInterface/Shape.java
@@ -1,4 +1,5 @@
 package myInterface;
+<<<<<<< HEAD
 
 
 /**
@@ -17,3 +18,23 @@ public interface Shape
      */
     int sampleMethod(int y);
 }
+=======
+/**
+ * 도형의 면적을 구해주는 interface.
+ *
+ * @author (2018315036 양유석, 2018315030 이가영, 2018315022 이지연)
+ * @version (2019.09.02)
+ */
+public interface Shape
+{
+    final double PI = 3.14;
+    void draw();
+    double getArea();
+    default public void redraw()
+    {
+         System.out.print("--- 다시 그림니다. ");
+         draw();
+    }
+}
+
+>>>>>>> d00503793867461ec25092fb1cd4d4c4e2803571
