a3d76546a75c6310fb52213be7109ab04362125e
leekayon <lky1732@gmail.com>
2019-09-09 13:56:20 +0900

    Shape 작성

--- myInterface/Shape.java
+++ myInterface/Shape.java
@@ -1,19 +1,19 @@
 package myInterface;
-
-
 /**
- * Write a description of interface Shape here.
+ * 도형의 면적을 구해주는 interface.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315036 양유석, 2018315030 이가영, 2018315022 이지연)
+ * @version (2019.09.02)
  */
 public interface Shape
 {
-    /**
-     * An example of a method header - replace this comment with your own
-     *
-     * @param  y a sample parameter for a method
-     * @return   the result produced by sampleMethod
-     */
-    int sampleMethod(int y);
+    final double PI = 3.14;
+    void draw();
+    double getArea();
+    default public void redraw()
+    {
+         System.out.print("--- 다시 그림니다. ");
+         draw();
+    }
 }
+
