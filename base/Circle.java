e813cdf49c2ddb4d34f1a2942fb57ccb535e478f
leekayon <lky1732@gmail.com>
2019-09-10 22:47:56 +0900

    boolean part 작성

--- base/Circle.java
+++ base/Circle.java
@@ -1,21 +1,28 @@
 package base;
 
- /*
-  * 
-  * 
-  * @author ((2018315036 양유석 , 2018315030 이가영 , 2018315022 이지연))
-  * @version (a version number or a date)
-  */
+/*
+ * 
+ * 
+ * @author ((2018315036 양유석 , 2018315030 이가영 , 2018315022 이지연))
+ * @version (a version number or a date)
+ */
 public class Circle
 {
     private int x;
     private int y;
     private int radius;
-    
+
     public Circle(int x, int y, int radius){
         this.x = x;
         this.y = y;
+        this.radius = radius;
     }
-    
 
-}
+    public boolean equals(Circle cir ){
+        if(){
+            return true;
+        }
+        else
+            return false;
+    }
+}   
