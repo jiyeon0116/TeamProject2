10201a0daeba0ee42ef7fabab80e8cd11e97412a
jiyeon <gbko159@naver.com>
2019-09-10 23:27:03 +0900

    Circle 클래스에 추상메소드 오버라이딩했습니다

--- base/Circle.java
+++ base/Circle.java
@@ -6,7 +6,7 @@ package base;
   * @author ((2018315036 양유석 , 2018315030 이가영 , 2018315022 이지연))
   * @version (a version number or a date)
   */
-public class Circle
+public class Circle implements Shape
 {
     private int x;
     private int y;
@@ -17,5 +17,10 @@ public class Circle
         this.y = y;
     }
     
-
+    public void draw(){
+    }
+    public void redraw(){
+    }
+    public void getArea(){
+    }
 }
